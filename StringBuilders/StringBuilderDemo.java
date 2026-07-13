05d451c2e9059b3c206aa1789fdd278c7a5eee4d
srirengasarathy13 <sri.rengasarathy.007@gmail.com>
2026-07-13 16:59:26 +0530

    String Builder Updated

---
+++ StringBuilders/StringBuilderDemo.java
@@ -0,0 +1,12 @@
+package StringBuilders;
+
+public class StringBuilderDemo {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("No.115, ");
+        sb.append("Sri Ragavendra Men's PG, ");
+        sb.append("Ambal Nagar, Ekkatuthangal");
+        sb.append(", Chennai - 600032");
+        System.out.println(sb);
+    }
+}
