3bed98e235683a802be31da1b2b6747abc19233a
srirengasarathy13 <sri.rengasarathy.007@gmail.com>
2026-07-16 14:32:21 +0530

    Immutable Updated

---
+++ Immutable.java
@@ -0,0 +1,8 @@
+public class Immutable{
+    public static void main(String[] args) {
+        String name = "Nikish";
+        name.concat(" Daniel");
+        System.out.println(name);
+    }
+}
+ 
