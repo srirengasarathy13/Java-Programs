83d03dfa0b9e5dcfd6044ac99ea9260f607c59bd
srirengasarathy13 <sri.rengasarathy.007@gmail.com>
2026-07-27 23:40:03 +0530

    System.in InputReader Updated

---
+++ LearntFromYt/SystemInRead.java
@@ -0,0 +1,18 @@
+package LearntFromYt;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SystemInRead {
+    public static void main(String[] args) throws IOException {
+        System.out.print("Enter a number : ");
+        // int num = System.in.read(); // returns the ASCI value of the input
+        // System.err.println(num);
+
+        InputStreamReader in = new InputStreamReader(System.in);
+        BufferedReader bf = new BufferedReader(in);
+        int num = Integer.parseInt(bf.readLine());
+        System.out.println(num);
+    }
+}
