ff049d06d9f5bfe3bb358821ae2ea3e601e864f2
srirengasarathy13 <sri.rengasarathy.007@gmail.com>
2026-08-11 15:27:16 +0530

    Annotation with Reflection - Updated

---
+++ Advanced_Core_Java/Reflection/HRMSAnnotationDemo.java
@@ -0,0 +1,30 @@
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@interface EmployeeInfo {
+    String department();
+    String role();
+}
+
+@EmployeeInfo(department = "SAP", role = "ABAP Developer")
+class Employee_AnnotationDemo {
+    String employeeId = "EMP1001";
+    String employeeName = "Rama";
+}
+
+public class HRMSAnnotationDemo {
+
+    public static void main(String[] args) {
+
+        Class<Employee_AnnotationDemo> employeeClass =
+                Employee_AnnotationDemo.class;
+
+        EmployeeInfo info =
+                employeeClass.getAnnotation(EmployeeInfo.class);
+
+        System.out.println("Employee ID     : EMP1001");
+        System.out.println("Employee Name   : Rama");
+        System.out.println("Department      : " + info.department());
+        System.out.println("Role            : " + info.role());
+    }
+}
